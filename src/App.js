f76cb49414d81831438b085570eff3776d2254cf
jbmenashi <jbmenashi@gmail.com>
2019-02-21 16:37:40 -0500

    Basic styling for container reference

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id="titleDiv"><h1 id="title">The Civil War</h1></div>
+        <div id="titleDiv">
+          <h1 id="title">Glory, Glory, Hallelujah!</h1>
+          <h6 id="bio">An Interactive Historical Timeline of the American Civil War</h6>
+          </div>
         <div><SliderContainer/></div>
         <div><MainContainer/></div>
       </div>
