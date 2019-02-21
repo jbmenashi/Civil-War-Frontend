852a4ed93ee7027d999e2c2d4a9d176f63fd6a8f
jbmenashi <jbmenashi@gmail.com>
2019-02-21 16:06:02 -0500

    create big containers

---
+++ src/containers/SliderContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SliderContainer extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default SliderContainer;
