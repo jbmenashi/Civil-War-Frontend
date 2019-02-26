c02441afcab9b0e413bfd9fe2fc1dc9d83b17622
jbmenashi <jbmenashi@gmail.com>
2019-02-26 12:00:50 -0500

    add slider component

--- src/containers/SliderContainer.js
+++ src/containers/SliderContainer.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Slider from '../components/Slider'
 
 const SliderContainer = () => {
   return (
-    <div id="sliderContainer"></div>
+    <div id="sliderContainer" className="container">
+      <div className="row">
+        <div className="col-1">
+          <button className="btn-sm btn-info">Play</button>
+        </div>
+        <div className="col-1">
+          <button className="btn-sm btn-info">Pause</button>
+        </div>
+        <div className="col-10">
+          <Slider/>
+        </div>
+      </div>
+    </div>
   );
 }
 
