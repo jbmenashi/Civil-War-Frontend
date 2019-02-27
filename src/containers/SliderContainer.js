9e2c4cca87c9b2147f4b1a7ff1218d813e46b273
jbmenashi <jbmenashi@gmail.com>
2019-02-27 13:58:50 -0500

    buttons working

--- src/containers/SliderContainer.js
+++ src/containers/SliderContainer.js
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import Slidey from '../components/Slidey'
 
-const SliderContainer = () => {
-  return (
-    <div id="sliderContainer" className="container">
-      <div className="row">
-        <div className="col-1">
-          <button className="btn-lg">Play</button>
-        </div>
-        <div className="col-1">
-          <button className="btn-lg">Pause</button>
-        </div>
-        <div className="col-10">
-          <Slidey/>
-        </div>
-      </div>
-    </div>
-  );
+const mapStateToProps = state => {
+  return {
+    sliderValue: state.slider.sliderValue
+  }
 }
 
-export default SliderContainer;
+const mapDispatchToProps = dispatch => {
+  return {
+    advance: () => dispatch({type: "ADVANCE"}),
+    retreat: () => dispatch({type: "RETREAT"})
+  }
+}
+
+let intervalId
+
+class SliderContainer extends Component {
+
+  play = () => intervalId = setInterval(this.props.advance, 2000);
+
+  pause = () => clearInterval(intervalId);
+
+  advance = () => {
+    clearInterval(intervalId);
+    this.props.advance()
+  }
+
+  retreat = () => {
+    clearInterval(intervalId);
+    this.props.retreat()
+  }
+
+  render() {
+    return (
+      <div id="sliderContainer" className="container">
+        <div className="row">
+          <div className="col-2">
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button className="btn-sm" onClick={() => this.play()}>PLAY</button>
+              <button className="btn-sm" onClick={() => this.pause()}>PAUSE</button>
+              <button className="btn-sm" onClick={() => this.advance()}>ADVANCE</button>
+              <button className="btn-sm" onClick={() => this.retreat()}>RETREAT</button>
+            </div>
+          </div>
+          <div className="col-10">
+            <Slidey/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SliderContainer);
--- src/reducers/slider.js
+++ src/reducers/slider.js
@@ -6,6 +6,10 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "SLIDE":
       return {...state, sliderValue: action.payload}
+    case "ADVANCE":
+      return {...state, sliderValue: state.sliderValue + 1}
+    case "RETREAT":
+      return {...state, sliderValue: state.sliderValue - 1}
     default:
       return state
   }
