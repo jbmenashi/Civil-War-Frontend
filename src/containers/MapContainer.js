df67b880b4da6d12f4ed7260b5d23e6a1ed56029
jbmenashi <jbmenashi@gmail.com>
2019-03-08 19:35:05 -0500

    stable map

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import ActualMap from '../components/ActualMap'
+import ReactMapboxGl, { ZoomControl } from "react-mapbox-gl";
 
-class MapContainer extends Component {
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoiamJtZW5hc2hpIiwiYSI6ImNqcWZxc2tpaDJ2MGc0M3FxdWxrdTF2eGUifQ.FVh60f1zW4orYek2_IjsKA"
+});
+
+class mapContainer extends Component {
 
   render() {
+    const center = [-65, 35]
+    const zoom = [4]
     return (
-      <div id="mapContainer">
-        <ActualMap/>
+      <div>
+        <Map
+          style='mapbox://styles/jbmenashi/cjsm7lutw4v0f1fqdq35gcbfe'
+          center={center}
+          zoom={zoom}
+          containerStyle={{
+            height: "77vh",
+            width: "143vw",
+            left: "-72vw",
+            margin: "1vw",
+          }}>
+          <ZoomControl />
+        </Map>
       </div>
     );
   }
 
 }
 
-export default MapContainer;
+export default mapContainer;
